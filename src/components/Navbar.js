28a739c4177c9134049dd257242b5edf1f93f78f
jzf21 <jozefneelamkavil2003@gmail.com>
2022-10-11 08:58:02 +0530

    navbar almost okay

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,43 +1,71 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import $ from 'jquery'
+
+
+$(document).ready(function () {
+
+  $('.first-button').on('click', function () {
+
+    $('.animated-icon1').toggleClass('open');
+  });
+  $('.nav-link').on('click',function(){
+    $('.navbar div').removeClass('show');
+    $('.animated-icon1').toggleClass('open');
+
+    
+});
+
+});
+
+
+
 
 export const Navbar = () => {
   return (
-    <nav class=" navbar navbar-expand-lg navbar-light z-3 shadow-sm bg-white fixed z-10">
-   
-    <div class="container-fluid">
-    <a class="navbar-brand d-lg-none title mx-auto" href="#">SPOT ON</a>
-      <button
-        class="navbar-toggler"
-        type="button"
-        data-bs-toggle="collapse"
-        data-bs-target="#navbar"
-      >
-        <span class="navbar-toggler-icon"></span>
-      </button>
-    
-    <div className="collapse navbar-collapse" id="navbar">
-        <ul className="navbar-nav   mx-auto gap-10">
-            <li className="nav-item md:mr-20 md:ml-12">
-                <a className="nav-link" href="#home">Home</a>
+    <nav class="  navbar navbar-expand-lg navbar-light z-20 shadow-sm bg-white  ">
+      <div class="container-fluid">
+        <div class=" nav-item title nav-item">
+          <a class="navbar-brand d-lg-none  mx-5 title " href="#">
+            SPOT ON
+          </a>
+        </div>
+        <button class="d-lg-none first-button mr-2" type="button" data-bs-toggle="collapse" data-bs-target="#navbar"
+    aria-controls="navbarSupportedContent20" aria-expanded="false" aria-label="Toggle navigation">
+    <div class="navbar-toggler animated-icon1"><span></span><span></span><span></span></div>
+  </button>
+
+
+        <div class="collapse navbar-collapse z-2" id="navbar">
+          <ul class="navbar-nav   mx-auto gap-10">
+            <li class="nav-item md:mr-20 md:ml-12" >
+              <a class="nav-link single" href="#home">
+                Home
+              </a>
             </li>
-            <li className="nav-item md:mr-20 md:ml-12">
-                <a className="nav-link" href="#about">About</a>
+            <li class="nav-item md:mr-20 md:ml-12">
+              <a class="nav-link single" href="#about">
+                About
+              </a>
             </li>
-            <li className=" nav-item title mx-[100px]">
-                <a className="nav-link title" href="#">SPOT ON</a>
+            <div class="  d-none d-md-block nav-item title mx-[60px] md:mx-[100px]">
+              <a class="nav-link title whitespace-nowrap  " href="#">
+                SPOT ON
+              </a>
+            </div>
+
+            <li class="nav-item md:mr-20 md:ml-20">
+              <a class="nav-link single" href="#phases">
+                Services
+              </a>
             </li>
-            <li className="nav-item md:mr-20 md:ml-20">
-                <a className="nav-link" href="#">Services</a>
+            <li class="nav-item md:mr-20">
+              <a class="nav-link single" href="#contact">
+                Contact
+              </a>
             </li>
-            <li className="nav-item md:mr-20">
-                <a className="nav-link" href="#">Contact</a>
-            </li>
-        </ul>
-    </div>
-    </div>
+          </ul>
+        </div>
+      </div>
     </nav>
-
-
-    
-  )
-}
+  );
+};
--- src/components/home.js
+++ src/components/home.js
@@ -8,13 +8,18 @@ export const Home = () => {
     <div className="">
      
       <div className="grid grid-rows-2">
-        <div className="justify-center absolute left-[537px] top-[170px]">
-          <img src={centerimg}/>
-               <button class="button1 text-white font-bold py-2 px-4 mt-2 rounded hover:bg-red-600">
+        <div className="flex flex-col absolute md:left-[537px] md:top-[170px]">
+          <div className="justify-center ">
+          <img src={centerimg} className=""/>
+              
+        </div>
+        <div className=" ml-5">
+           <button class=" button1 text-white font-bold py-2 px-4 mt-2 rounded hover:bg-red-600">
             REGISTER NOW
           </button>
         </div>
-        <div className="absolute ml-5 md:left-[6cd00px] xl:left-[952px] top-[200px] xl:md:top-[270px] px-2">
+        </div>
+        <div className="absolute ml-5 md:left-[600px] xl:left-[952px] top-[200px] xl:md:top-[270px] px-2">
           <p className="maintext ">
             MAKE YOUR <br />
             VOICE COUNT!
@@ -24,7 +29,7 @@ export const Home = () => {
      
         </div>
         <div className=" -z-10 w-full   ">
-          <img src={mainbg} className="mainbg absolute top-0 " />
+          <img src={mainbg} className="mainbg bg-contain md:bg-auto absolute top-0 " />
       
         </div>
       </div>
